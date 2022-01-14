79462fcf73f8f07bbd9f6aaf50498b8c05fc5a6f
Your Name <you@example.com>
2022-01-14 19:41:57 +0100

    init

---
+++ HelloWorld/src/com/jim/Main.java
@@ -0,0 +1,11 @@
+package com.jim;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! I'm your first java program!");
+        MainFrame myFrame = new MainFrame();
+        myFrame.init();
+
+    }
+}
---
+++ HelloWorld/src/com/jim/MainFrame.java
@@ -0,0 +1,12 @@
+package com.jim;
+
+import javax.swing.*;
+
+public class MainFrame extends JFrame {
+    public void init() {
+        setTitle("Hello every body");
+        setSize(450, 300);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
